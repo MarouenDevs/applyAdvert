59d09f38ab0b56ea63397cee87ef89044bfc574a
marouen <marouen.ben.ahmed1@gmail.com>
2017-05-19 09:56:05 +0100

    layout global montage template

--- src/AppBundle/Controller/PagesController.php
+++ src/AppBundle/Controller/PagesController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class DefaultController extends Controller
+class PagesController extends Controller
 {
 
 
